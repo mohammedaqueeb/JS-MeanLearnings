c3ce54e55e03ec2f1a710f3316a5c0b0ae9689e8
aqueeb shaikh <aqueeb96@gmail.com>
2025-04-25 15:37:36 +0530

    added latest questions

--- TS/typeinfernce.ts
+++
@@ -1,3 +0,0 @@
-//Here, since we are not explicitly defining a: string with a type annotation, TypeScript infers the type of the variable based on the value assigned to the variable. The value of a is a string and hence the type of a is inferred as string.
-
-let a: string;
--- TS/union.ts
+++
@@ -1,4 +0,0 @@
-// | stand for union
-var value: number | boolean;
-value = 0;
-value = true;
